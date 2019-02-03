7821581fb75cbda8bc477f2105a40eeb1d03a658
Evan X. Merz <evanxmerz@yahoo.com>
2019-02-03 13:55:37 -0800

    Added an example to show how to search by tag

---
+++ src/com/algorithmicaudio/freesound/example/SearchByTag.java
@@ -0,0 +1,43 @@
+package com.algorithmicaudio.freesound.example;
+
+import javax.security.auth.login.CredentialNotFoundException;
+import com.algorithmicaudio.freesound.FreeSound;
+import com.algorithmicaudio.freesound.response.SearchResponse;
+
+/*
+ * This example demonstrates how to search for sounds using a tag.
+ */
+public class SearchByTag
+{
+	public static void main(String[] args) throws CredentialNotFoundException
+	{
+		new SearchByTag();
+	}
+	
+	public SearchByTag() throws CredentialNotFoundException
+	{
+		/*
+		 * These are the FreeSound API credentials for your app.
+		 * Go to this URL to apply: https://freesound.org/help/developers/
+		 */
+		String clientSecret = ""; // The client secret for YOUR freesound app.
+		
+		/*
+		 * Initialize the FreeSound client.
+		 */
+		FreeSound freeSoundClient = new FreeSound(clientSecret);
+		
+		/*
+		 * Send a text search request to the FreeSound API (website).
+		 */
+		SearchResponse results = freeSoundClient.searchByTag("glass");
+		
+		/*
+		 * Print the results.
+		 */
+		for(int i = 0; i < results.count(); i++)
+		{
+			System.out.println(results.get(i).toString());
+		}
+	}
+}
